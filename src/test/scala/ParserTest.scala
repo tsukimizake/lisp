e8f9e445ced2b97ee5766dd35afd5ba138be0bae
Shojin <shomasd@gmail.com>
2020-05-25 11:49:02 +0900

    remove confused tests

--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -1,19 +1,6 @@
 class ParserTest extends org.scalatest.FunSuite {
-  test("symbol eq") {
-    val x = new Symbol("eu")
-    val y = new Symbol("eu")
-    assert(x == y)
-
-  }
-  test("symbol list eq") {
-    assert(List(Symbol("eu")) == List(Symbol("eu")))
-  }
   test("symbol token") {
-    // これが通らない
-    // 左辺がToken型で右辺がSymbol型だと比較がうまくいかない
-    val r: Token = new Symbol("honi")
-    assert(Parser.tokenize("honi")(0) == r)
-
+    assert(Parser.tokenize("honi")(0) == new Symbol("honi"))
   }
   test("(a test)") {
     Parser.parseExpr("(a test)")
